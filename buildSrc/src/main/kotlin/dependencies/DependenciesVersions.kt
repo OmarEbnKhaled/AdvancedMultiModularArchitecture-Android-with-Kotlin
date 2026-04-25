70d3d549cb005eade90564371bb9ca3a0db6cbd8
OmarEbnKhaled <omarabdelaal2013@gmail.com>
2026-04-25 12:10:12 +0300

    Lect-34-Test-Shared-Library-Gradle-Plugin

--- buildSrc/src/main/kotlin/dependencies/DependenciesVersions.kt
+++ buildSrc/src/main/kotlin/dependencies/DependenciesVersions.kt
@@ -4,6 +4,7 @@ object DependenciesVersions {
     const val CORE_KTX = "1.10.1"
     const val LIFE_CYCLE_RUNTIME_KTX = "2.6.1"
     const val KOTLIN = "1.9.0"
+    const val KOTLIN_COMPILER = "1.5.1"
     const val JUNIT = "4.13.2"
     const val JUNIT_VERSION = "1.1.5"
     const val ESPRESSO_CORE = "3.5.1"
