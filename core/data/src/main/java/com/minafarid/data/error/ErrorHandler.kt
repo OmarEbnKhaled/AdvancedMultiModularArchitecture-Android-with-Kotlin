b69121ef5ee1638fb3001838a91634c03d93a3f8
OmarEbnKhaled <omarabdelaal2013@gmail.com>
2026-04-28 17:25:15 +0300

    Lect-77-Effective-Error-Handling-Mapping-Responses-to-Domain-Models

---
+++ core/data/src/main/java/com/minafarid/data/error/ErrorHandler.kt
@@ -0,0 +1,29 @@
+package com.minafarid.data.error
+
+import com.google.gson.Gson
+import com.minafarid.data.model.ErrorMessage
+import com.minafarid.data.response.ErrorResponse
+
+// Mapping errorResponse to ErrorMessage model
+fun ErrorResponse.toDomain(code: Int): ErrorMessage {
+    return ErrorMessage(
+        code = code,
+        message = errorMessage.orEmpty(),
+        errorFieldList = errorFieldList ?: emptyList(),
+    )
+}
+
+// Create default error response
+fun defaultErrorResponse() = ErrorResponse(
+    errorCode = "",
+    errorMessage = "",
+    errorFieldList = emptyList()
+)
+
+// Getting error response from error body "string"
+fun getErrorResponse(gson: Gson, errorBodyString: String): ErrorResponse =
+    try {
+        gson.fromJson(errorBodyString, ErrorResponse::class.java)
+    } catch (e: Exception) {
+        defaultErrorResponse()
+    }
