6151201405f7188f995ca3f6e7b0e642f4883140
OmarEbnKhaled <omarabdelaal2013@gmail.com>
2026-04-30 11:49:03 +0300

    Lect-93-Add-PersistentList-Serializer-for-DataStore-Settings

--- app/src/main/java/com/minafarid/advancedmultimodulararchitecture/MainActivity.kt
+++ app/src/main/java/com/minafarid/advancedmultimodulararchitecture/MainActivity.kt
@@ -27,7 +27,6 @@ import com.minafarid.datastore.settings.AppSettings
 import com.minafarid.datastore.settings.AppSettingsSerializer
 import com.minafarid.datastore.settings.Language
 import com.minafarid.datastore.settings.Location
-import com.minafarid.provider.DataProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -35,114 +34,116 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var appSettingDataStore: DataStore<AppSettings>
+  lateinit var appSettingDataStore: DataStore<AppSettings>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        appSettingDataStore = DataStoreFactory.create(
-            serializer = AppSettingsSerializer(),
-            produceFile = { dataStoreFile("app_setting.json") },
-            scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-        )
+    appSettingDataStore = DataStoreFactory.create(
+      serializer = AppSettingsSerializer(),
+      produceFile = { dataStoreFile("app_setting.json") },
+      scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+    )
 
-        // val room = Room.databaseBuilder()
+    // val room = Room.databaseBuilder()
 
-        enableEdgeToEdge()
-        setContent {
-            AdvancedMultiModularArchitectureTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = DataProvider.userName,
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
-            }
+    enableEdgeToEdge()
+    setContent {
+      AdvancedMultiModularArchitectureTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = DataProvider.userName,
+//                        modifier = Modifier.padding(innerPadding),
+//                    )
+
+          SettingScreen(appSettingDataStore, modifier = Modifier.padding(innerPadding))
         }
-
-        val counter = 100
-
-        print(counter)
+      }
     }
 
-    fun mainMainMainMainMainMainMainMainMainMainMainMainMainMainMainMain() {
-    }
+    val counter = 100
+
+    print(counter)
+  }
+
+  fun mainMainMainMainMainMainMainMainMainMainMainMainMainMainMainMain() {
+  }
 }
 
 @Composable
 fun SettingScreen(appSettingDataStore: DataStore<AppSettings>, modifier: Modifier) {
-    val scope = rememberCoroutineScope()
-    val appSetting by appSettingDataStore.data.collectAsState(initial = AppSettings())
+  val scope = rememberCoroutineScope()
+  val appSetting by appSettingDataStore.data.collectAsState(initial = AppSettings())
 
-    Column(modifier = Modifier.padding(50.dp)) {
-        // Display saved language
-        Text(
-            text = "Language ${appSetting.language}, \nFrom (Data Store).",
-            modifier = modifier,
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+  Column(modifier = Modifier.padding(50.dp)) {
+    // Display saved language
+    Text(
+      text = "Language ${appSetting.language}, \nFrom (Data Store).",
+      modifier = modifier,
+    )
+    Spacer(modifier = Modifier.height(16.dp))
 
-        // Display saved Location
-        Text(
-            text = "Last Known Location: \nFrom (Data Store).",
-            modifier = modifier,
-        )
-        appSetting.lastKnownLocations.forEach { location ->
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Lat: ${location.lat}, Long: ${location.long}")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Create drop down menu to display language options and set location as well
-        val newLocation = Location(37.123, 122.908)
-        Language.entries.forEach { language ->
-            DropdownMenuItem(text = { Text(text = language.name) }, onClick = {
-                scope.launch {
-                    appSettingDataStore.updateData { currentSettings ->
-                        currentSettings.copy(
-                            language = language,
-                            lastKnownLocations = currentSettings.lastKnownLocations.add(newLocation)
-                        )
-                    }
-                }
-            })
-        }
-        Spacer(modifier = Modifier.height(16.dp))
+    // Display saved Location
+    Text(
+      text = "Last Known Location: \nFrom (Data Store).",
+      modifier = modifier,
+    )
+    appSetting.lastKnownLocations.forEach { location ->
+      Spacer(modifier = Modifier.height(16.dp))
+      Text(text = "Lat: ${location.lat}, Long: ${location.long}")
     }
+    Spacer(modifier = Modifier.height(16.dp))
+
+    // Create drop down menu to display language options and set location as well
+    val newLocation = Location(37.123, 122.908)
+    Language.entries.forEach { language ->
+      DropdownMenuItem(text = { Text(text = language.name) }, onClick = {
+        scope.launch {
+          appSettingDataStore.updateData { currentSettings ->
+            currentSettings.copy(
+              language = language,
+              lastKnownLocations = currentSettings.lastKnownLocations.add(newLocation),
+            )
+          }
+        }
+      })
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+  }
 }
 
 @Composable
 @Suppress("FunctionNaming")
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(
-            text = "Hello $name, \nFrom (DataProvider.kt).",
-            modifier = modifier,
-        )
-        Text(
-            text = "Base Url: ${BuildConfig.BASE_URL}, \nFrom (BuildConfig.java).",
-            modifier = modifier,
-        )
-        Text(
-            text = "Can Clear Cash: ${BuildConfig.CAN_CLEAR_CASH}, \nFrom (BuildConfig.java).",
-            modifier = modifier,
-        )
-        Text(
-            text = "DB Version: ${BuildConfig.DB_VERSION}, \nFrom (BuildConfig.java).",
-            modifier = modifier,
-        )
-        Text(
-            text = "Map Key: ${BuildConfig.MAP_KEY}, \nFrom (BuildConfig.java).",
-            modifier = modifier,
-        )
-    }
+  Column(modifier = modifier) {
+    Text(
+      text = "Hello $name, \nFrom (DataProvider.kt).",
+      modifier = modifier,
+    )
+    Text(
+      text = "Base Url: ${BuildConfig.BASE_URL}, \nFrom (BuildConfig.java).",
+      modifier = modifier,
+    )
+    Text(
+      text = "Can Clear Cash: ${BuildConfig.CAN_CLEAR_CASH}, \nFrom (BuildConfig.java).",
+      modifier = modifier,
+    )
+    Text(
+      text = "DB Version: ${BuildConfig.DB_VERSION}, \nFrom (BuildConfig.java).",
+      modifier = modifier,
+    )
+    Text(
+      text = "Map Key: ${BuildConfig.MAP_KEY}, \nFrom (BuildConfig.java).",
+      modifier = modifier,
+    )
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 @Suppress("FunctionNaming")
 fun GreetingPreview() {
-    AdvancedMultiModularArchitectureTheme {
-        Greeting("Android")
-    }
+  AdvancedMultiModularArchitectureTheme {
+    Greeting("Android")
+  }
 }
